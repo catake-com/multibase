eaec71565ab41be3c3fe771d8718c508d822cdf4
Slava Dzyba <slavdz@gmail.com>
2023-04-23 22:23:16 +0200

    Move Kafka consuming to a separate component

--- frontend/src/components/Kafka.vue
+++ frontend/src/components/Kafka.vue
@@ -2,6 +2,7 @@
 import { computed } from "vue";
 import { useQuasar } from "quasar";
 import { useKafkaStore } from "../stores/kafka";
+import Consume from "./kafka/Consume.vue";
 
 const quasar = useQuasar();
 
@@ -12,7 +13,6 @@ const props = defineProps({
 });
 
 const splitterWidth = 12;
-const splitterWidthConsuming = 12;
 
 await kafkaStore.loadProject(props.projectID);
 
@@ -76,21 +76,10 @@ const authPassword = computed({
   },
 });
 
-const hoursAgo = computed({
-  get() {
-    return kafkaStore.consumingSession(props.projectID).hoursAgo;
-  },
-  async set(hoursAgo) {
-    kafkaStore.consumingSession(props.projectID).hoursAgo = hoursAgo;
-  },
-});
-
 const currentTopic = computed(() => kafkaStore.consumingSession(props.projectID).currentTopic);
 const topics = computed(() => kafkaStore.topicsData(props.projectID));
 const brokers = computed(() => kafkaStore.brokersData(props.projectID));
 const consumers = computed(() => kafkaStore.consumersData(props.projectID));
-const consumedTopic = computed(() => kafkaStore.consumedTopic(props.projectID));
-const consumedTopicMessages = computed(() => kafkaStore.consumedTopicMessages(props.projectID));
 
 async function connect() {
   try {
@@ -115,85 +104,12 @@ async function startTopicConsuming(topic) {
     quasar.notify({ type: "negative", message: error });
   }
 }
-
-async function stopTopicConsuming() {
-  try {
-    await kafkaStore.stopTopicConsuming(props.projectID);
-    quasar.notify({ type: "positive", message: "Stopped consuming" });
-  } catch (error) {
-    quasar.notify({ type: "negative", message: error });
-  }
-}
-
-async function restartTopicConsuming() {
-  try {
-    await kafkaStore.restartTopicConsuming(props.projectID);
-    quasar.notify({ type: "positive", message: "Restarted consuming" });
-  } catch (error) {
-    quasar.notify({ type: "negative", message: error });
-  }
-}
-
-function consumedMessagesTableRowKey(row) {
-  return `${row.partitionID}_${row.offset}`;
-}
-
-const consumedMessagesTableColumns = [
-  {
-    name: "timestamp",
-    label: "Timestamp",
-    align: "left",
-    field: "timestamp",
-  },
-  { name: "partition", align: "left", label: "Partition", field: "partitionID" },
-  { name: "offset", align: "left", label: "Offset", field: "offset" },
-  { name: "key", align: "left", label: "Key", field: "key" },
-  { name: "data", align: "left", label: "Data", field: "data" },
-  { name: "headers", align: "left", label: "Headers", field: "" },
-];
-
-const consumedMessagesTablePagination = {
-  rowsPerPage: 20,
-};
-
-const consumedMessagesTableRowsPerPage = [5, 10, 20, 50, 100, 200, 500];
 </script>
 
 <template>
   <div class="full-height">
     <div v-if="currentTopic">
-      <q-btn label="Stop" color="secondary" @click="stopTopicConsuming()" />
-
-      <q-splitter v-model="splitterWidthConsuming" class="full-height" disable>
-        <template v-slot:before>
-          {{ currentTopic }}
-
-          <q-input v-model="hoursAgo" label="Show messages for last X hours" />
-
-          <!--          <div>Count total: {{ consumedTopic?.countTotal }}</div>-->
-          <!--          Partitions:-->
-          <!--          <q-list>-->
-          <!--            <q-item v-for="partition in consumedTopic?.partitions" :key="partition.id">-->
-          <!--              <q-item-section>-->
-          <!--                <q-item-label overline>{{ partition.id }}</q-item-label>-->
-          <!--                <q-item-label>{{ partition.offsetTotalStart }} - {{ partition.offsetTotalEnd }}</q-item-label>-->
-          <!--              </q-item-section>-->
-          <!--            </q-item>-->
-          <!--          </q-list>-->
-
-          <q-btn label="Refresh" color="secondary" @click="restartTopicConsuming()" />
-        </template>
-
-        <template v-slot:after>
-          <q-table
-            :rows="consumedTopicMessages"
-            :columns="consumedMessagesTableColumns"
-            :row-key="consumedMessagesTableRowKey"
-            :pagination="consumedMessagesTablePagination"
-            :rows-per-page-options="consumedMessagesTableRowsPerPage"
-          />
-        </template>
-      </q-splitter>
+      <Consume :projectID="projectID"></Consume>
     </div>
 
     <div v-else>
