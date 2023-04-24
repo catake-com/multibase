7bc5b175fda98c8d288c4340ee43d91e499e3db1
Slava Dzyba <slavdz@gmail.com>
2023-04-24 17:35:06 +0200

    Remove frontend console logging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,10 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
-import terminal from "vite-plugin-terminal";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    terminal({
-      console: "terminal",
-      output: ["terminal", "console"],
-    }),
-
     vue({
       template: { transformAssetUrls },
     }),
